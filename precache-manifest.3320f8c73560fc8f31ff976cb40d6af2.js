bf0e65739fcab5fe42e599387e2ea9b62888ed93
luckydaypunk <christopher.greene24@mail.dcu.ie>
2019-02-09 16:45:07 +0900

    Updates

---
+++ precache-manifest.3320f8c73560fc8f31ff976cb40d6af2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "df4f688de4ccc539624fd6a8e8853cfb",
+    "url": "/static/media/sayajin_me.df4f688d.jpg"
+  },
+  {
+    "revision": "cf47b6bbfa4e355708fcbd40be5609c7",
+    "url": "/static/media/me.cf47b6bb.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "54770b914a1887b2c292",
+    "url": "/static/js/main.54770b91.chunk.js"
+  },
+  {
+    "revision": "90107ab562fd695576ea",
+    "url": "/static/js/1.90107ab5.chunk.js"
+  },
+  {
+    "revision": "54770b914a1887b2c292",
+    "url": "/static/css/main.6a2ede15.chunk.css"
+  },
+  {
+    "revision": "714a378cb70a086331520e26c90bbbdc",
+    "url": "/index.html"
+  }
+];
