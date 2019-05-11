fc3440d56d3fe91d43874e1d8bd70942bdc7d838
luckydaypunk <christopher.greene24@mail.dcu.ie>
2019-05-11 21:56:57 +0900

    Updates

---
+++ precache-manifest.96bf172a617a7a6b7dcf3fc7e13df1ad.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "7daa0cce6945cc12a9304524987a16a8",
+    "url": "/static/media/turing.7daa0cce.jpg"
+  },
+  {
+    "revision": "df4f688de4ccc539624fd6a8e8853cfb",
+    "url": "/static/media/sayajin_me.df4f688d.jpg"
+  },
+  {
+    "revision": "cf47b6bbfa4e355708fcbd40be5609c7",
+    "url": "/static/media/me.cf47b6bb.jpg"
+  },
+  {
+    "revision": "fe050de8d772832c4022a155bb26f744",
+    "url": "/static/media/coffee.fe050de8.jpg"
+  },
+  {
+    "revision": "e324e3a1a8e7a8c886ce785a9f680a24",
+    "url": "/static/media/LondonBus.e324e3a1.JPG"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e4e329289ff5e00f2310",
+    "url": "/static/js/main.e4e32928.chunk.js"
+  },
+  {
+    "revision": "369e6e459b301df33aa0",
+    "url": "/static/js/1.369e6e45.chunk.js"
+  },
+  {
+    "revision": "e4e329289ff5e00f2310",
+    "url": "/static/css/main.4ac76a49.chunk.css"
+  },
+  {
+    "revision": "f35087bf04890e53a72f636b85b4a004",
+    "url": "/index.html"
+  }
+];
